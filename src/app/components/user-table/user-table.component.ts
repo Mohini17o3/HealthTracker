8ac26506d6fee953fbf437f20b7544539e288c53
Mohini <upretimohini@gmail.com>
2024-07-21 18:23:42 +0530

    added filter

--- src/app/components/user-table/user-table.component.ts
+++ src/app/components/user-table/user-table.component.ts
@@ -1,23 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../../../services/user.service';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import {ReactiveFormsModule , FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-user-table',
   standalone: true,
-  imports: [CommonModule , MatPaginatorModule],
+  imports: [CommonModule, MatPaginatorModule, ReactiveFormsModule],
   templateUrl: './user-table.component.html',
-  styleUrl: './user-table.component.css'
+  styleUrls: ['./user-table.component.css']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, AfterViewInit {
   users: any[] = [];
+  filteredUsers: any[] = [];
+  paginatedRow: any[] = [];
+  length = 0;
+  pageSize = 5;
+  pageSizeOptions: number[] = [5, 10, 15, 20, 40, 100];
+  filterControl = new FormControl('');
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.userService.users$.subscribe(users => {
       this.users = users;
+      this.filteredUsers = users; 
+      this.length = users.length;
+      this.paginatedRowFunction({ pageIndex: 0, pageSize: this.pageSize } as PageEvent);
     });
+
+    this.filterControl.valueChanges.subscribe(value => {
+      this.filterRowFunction(value || '');
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.paginator.page.subscribe((pageEvent: PageEvent) => this.paginatedRowFunction(pageEvent));
+  }
+
+  paginatedRowFunction(pageEvent: PageEvent): void {
+    const startIndex = pageEvent.pageIndex * pageEvent.pageSize;
+    const endIndex = startIndex + pageEvent.pageSize;
+    this.paginatedRow = this.filteredUsers.slice(startIndex, endIndex);
+  }
+
+  filterRowFunction(filterValue: string): void {
+    const trimmed = filterValue.trim().toLowerCase();
+    this.filteredUsers = this.users.filter(user =>
+      user.name.toLowerCase().includes(trimmed)
+    );
+
+    this.length = this.filteredUsers.length;
+
+    if (this.paginator) {
+      this.paginator.pageIndex = 0; 
+      this.paginatedRowFunction({ pageIndex: 0,  pageSize: this.paginator.pageSize} as PageEvent); 
+    }
   }
 }
